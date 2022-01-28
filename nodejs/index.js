5b91192dc6ae952943e988233efa4f48cc927145
roqueang <roquem1@miumg.edu.gt>
2022-01-27 23:46:48 -0600

    master

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,7 +1,22 @@
 const http = require("http");
+const url = require("url");
 
 const callbackDelServidor =(req,res) => {
-  res.end('Hola mundo es mi primer servidor');
+  
+  //1. obtener url desde el objeto request
+  const urlActual = req.url;
+  const urlParseada = url.parse(urlActual, true);
+
+  //2. obtener la ruta
+  const ruta = urlParseada.pathname;
+
+  //3. enviar una respuesta dependiendo de la ruta
+if(ruta === '/ruta'){
+  res.end('estas en /ruta');
+} else{
+  res.end('estas en una ruta desconocida');
+}
+
 };
 
 const server = http.createServer(callbackDelServidor);
