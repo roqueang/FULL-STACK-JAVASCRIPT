12a9db6b6ed24f8597f4a4f83c8af82a60f878af
roqueang <roquem1@miumg.edu.gt>
2022-01-28 23:16:25 -0600

    master

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const url = require("url");
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const callbackDelServidor =(req,res) => {
   
@@ -10,14 +11,60 @@ const callbackDelServidor =(req,res) => {
   //2. obtener la ruta
   const ruta = urlParseada.pathname;
 
-  //3. enviar una respuesta dependiendo de la ruta
-if(ruta === '/ruta'){
-  res.end('estas en /ruta');
-} else{
-  res.end('estas en una ruta desconocida');
-}
+  //3. quitar slash
+  const rutaLimpia = ruta.replace(/^\/+|\/+$/g, '');
 
+  //3.1 obtener método http
+  const metodo = req.method.toLowerCase();
+
+  //3.2 obtener variables del query url
+  const { query = {} } = urlParseada;
+  console.log(query);
+
+  //3.3 obtener headers
+  const { headers = {}} = req;
+
+  //3.4 obtener payload, en el caso que haya uno
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  //3.4.1 ir acomulando la data cuanto request reciba un payload
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  //3.4.2 terminar de acomular datos y decirle al decoder que finalice.
+  req.on('end', () => {
+    buffer += decoder.end();
+  
+  //3.5 ordenar la data del request
+  const data = {
+    ruta: rutaLimpia,
+    query,
+    metodo,
+    headers,
+    payload: buffer
+  };
+
+    //4. enviar una respuesta dependiendo de la ruta
+  switch(rutaLimpia){
+    case 'ruta':
+      res.end('Estas en la ruta correcata');
+      break;
+    default:
+      res.end('Desconocido');
+    }
+  });
 };
+
+const enrutador ={
+  ruta: (data, callback) =>{
+    callback(200,{mensaje: 'estas en /ruta'} );
+  },
+  noEncontrado: (data, callback) =>{
+    callback(404,{mensaje: 'no encontrado'});
+  }
+}
 
 const server = http.createServer(callbackDelServidor);
 
