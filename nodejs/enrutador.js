948fe949d8a79375c1533712ab6183027a7e58ca
Angel Roque <angel.roquemorataya@gmail.com>
2022-02-08 15:31:21 -0600

    rutas y handler de veterinarios

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -1,11 +1,14 @@
 const recursos =require('./recursos');
 const mascotas =require('./rutas/mascotas');
+const veterinarias =require('./rutas/veterinarias');
 
 module.exports ={
     ruta: (data, callback) =>{
       callback(200,{mensaje: 'estas en /ruta'} );
     },
     mascotas: mascotas(recursos.mascotas),
+    veterinarias: veterinarias(recursos.veterinarias),
+    
     noEncontrado: (data, callback) =>{
       callback(404,{mensaje: 'no encontrado'});
     },
--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -8,5 +8,14 @@ module.exports  = {
     {
         tipo: "Perro",nombre: "Shadow2",dueno:"Angel"
     }
-]
+    ], 
+    veterinarias: [
+        {
+          identificacion:"0001", pais: "Guatemala", nombre: "Angel", apellido:"Roque"
+        },
+        {
+          identificacion:"0034", pais: "Colombia",  nombre: "Jhon", apellido: "Flores"
+        }
+    ]
+
   };
